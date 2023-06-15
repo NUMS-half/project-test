448a6674b4febf7fa2774887783ba39dd7df22e5
nums <1272606165@qq.com>
2023-06-16 00:08:37 +0800

    1

--- src/main/java/com/sisp/DemoApplication.java
+++ src/main/java/com/sisp/DemoApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.sisp.dao")
 public class DemoApplication {
 
-    //debug version
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
