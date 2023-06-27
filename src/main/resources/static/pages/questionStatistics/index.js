c3888dc36f12c3232b6abb8f5252169d4411441a
lhj <1983485901@qq.com>
2023-06-27 09:40:04 +0800

    lhj改的

---
+++ src/main/resources/static/pages/questionStatistics/index.js
@@ -0,0 +1,10 @@
+function generateSimilarQuestionStatistics() {
+    // Add your logic here to generate statistics for similar questions
+    // You can modify this code to fetch data from an API or calculate statistics based on your requirements
+
+    // Example code to display a sample statistic
+    const statisticContainer = document.getElementById('similar-questions');
+    const statisticElement = document.createElement('div');
+    statisticElement.textContent = 'Statistics for Similar Questions';
+    statisticContainer.appendChild(statisticElement);
+}
--- src/test/java/com/sisp/controller/QuestionnaireControllerTest.java
+++ src/test/java/com/sisp/controller/QuestionnaireControllerTest.java
@@ -1,15 +1,33 @@
 package com.sisp.controller;
 
 import com.sisp.beans.HttpResponseEntity;
+import com.sisp.dao.entity.OptionEntity;
+import com.sisp.dao.entity.QuestionEntity;
 import com.sisp.dao.entity.QuestionnaireEntity;
+import com.sisp.service.OptionService;
+import com.sisp.service.QuestionService;
+import com.sisp.service.QuestionnaireService;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Date;
+import java.io.IOException;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,8 +35,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class QuestionnaireControllerTest {
 
+    @InjectMocks
     @Autowired
     private QuestionnaireController questionnaireController;
+
+    @Mock
+    @Autowired
+    private QuestionnaireService questionnaireService;
+
+    @Mock
+    private QuestionService questionService;
+    @Mock
+    private OptionService optionService;
+    private MockMvc mockMvc;
+    private MockHttpSession session;
+    private MockHttpServletResponse response;
+
+    @Before
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(questionnaireController).build();
+    }
 
     /**
      * 测试查询问卷列表
@@ -56,8 +92,8 @@ public class QuestionnaireControllerTest {
         questionnaire1.setEndTime(new Date());
 
         questionnaire2.setQuestionnaireName("aaaaaaaaaasssssssssssssaaassssssaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
-                                            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
-                                            "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+                "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
         questionnaire2.setQuestionnaireDescription("测试");
         questionnaire2.setCreatedBy("admin");
         questionnaire2.setCreationDate(new Date());
@@ -73,4 +109,75 @@ public class QuestionnaireControllerTest {
         assertEquals("666", result1.getCode());
         assertEquals("0", result2.getCode());
     }
+
+    /**
+     * 测试逻辑删除问卷
+     */
+    @Test
+    @Transactional
+    public void deleteQuestionnaire() {
+        QuestionnaireEntity questionnaire1 = new QuestionnaireEntity();
+        QuestionnaireEntity questionnaire2 = new QuestionnaireEntity();
+        questionnaire1.setId("a8954cbf264e448599302d736ca88c86");
+        questionnaire2.setId("8888888");
+
+        HttpResponseEntity result1 = questionnaireController.deleteQuestionnaire(questionnaire1);
+        HttpResponseEntity result2 = questionnaireController.deleteQuestionnaire(questionnaire2);
+
+        //assertion
+        assertEquals("666", result1.getCode());
+        assertEquals("0", result2.getCode());
+        ;
+    }
+
+    /**
+     * 测试发布/收回问卷成功
+     */
+    @Test
+    @Transactional
+    public void testSetQuestionnaireStatus_publishSuccess() {
+        // 创建一个测试用的 QuestionnaireEntity 对象
+        QuestionnaireEntity questionnaire = new QuestionnaireEntity();
+        questionnaire.setStatus(0);
+
+        // 模拟 questionnaireService.modifyQuestionnaireInfo 方法返回成功
+        Mockito.when(questionnaireService.modifyQuestionnaireInfo(questionnaire)).thenReturn(1);
+
+        // 调用 setQuestionnaireStatus 方法
+        HttpResponseEntity response = questionnaireController.setQuestionnaireStatus(questionnaire);
+
+        // 验证返回的 HttpResponseEntity 对象是否符合预期
+        Assert.assertEquals("666", response.getCode());
+        Assert.assertEquals(questionnaire.getId(), response.getData());
+        Assert.assertEquals("发布成功", response.getMessage());
+
+        // 验证 questionnaireService.modifyQuestionnaireInfo 方法是否被调用
+        Mockito.verify(questionnaireService, Mockito.times(1)).modifyQuestionnaireInfo(questionnaire);
+    }
+
+    /**
+     * 测试发布/收回问卷失败
+     */
+    @Test
+    @Transactional
+    public void testSetQuestionnaireStatus_publishFailure() {
+        // 创建一个测试用的 QuestionnaireEntity 对象
+        QuestionnaireEntity questionnaire = new QuestionnaireEntity();
+        questionnaire.setStatus(0);
+
+        // 模拟 questionnaireService.modifyQuestionnaireInfo 方法返回失败
+        Mockito.when(questionnaireService.modifyQuestionnaireInfo(questionnaire)).thenReturn(0);
+
+        // 调用 setQuestionnaireStatus 方法
+        HttpResponseEntity response = questionnaireController.setQuestionnaireStatus(questionnaire);
+
+        // 验证返回的 HttpResponseEntity 对象是否符合预期
+        Assert.assertEquals("0", response.getCode());
+        Assert.assertEquals(0, response.getData());
+        Assert.assertEquals("发布失败", response.getMessage());
+
+        // 验证 questionnaireService.modifyQuestionnaireInfo 方法是否被调用
+        Mockito.verify(questionnaireService, Mockito.times(1)).modifyQuestionnaireInfo(questionnaire);
+    }
+
 }
